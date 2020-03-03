72b884a979d4a5ccafab2d3f7e7a81916297fdbb
kamipatel@gmail.com <kamlesh.patel@kampatel-ltm.internal.salesforce.com>
2020-03-02 19:58:30 -0600

    My first action is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,7 +23,7 @@ echo $@
 
 # install google chrome
 echo "install google chrome"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - 
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub 
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 apt-get -y update
 apt-get install -y google-chrome-stable
