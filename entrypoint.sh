582014980aed8838e3d6afa35f9399e680e74d5d
kamipatel@gmail.com <kamlesh.patel@kampatel-ltm.internal.salesforce.com>
2020-03-02 20:00:54 -0600

    My first action is ready

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,24 +21,6 @@ echo "jmeter args=$@"
 echo $@
 #jmeter $@
 
-# install google chrome
-echo "install google chrome"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub 
-sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-apt-get -y update
-apt-get install -y google-chrome-stable
-
-# install chromedriver
-echo "install chromedriver"
-apt-get install -yqq unzip
-wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
-unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
-
-echo "chrome webdriver installed!"
-
-# set display port to avoid crash
-ENV DISPLAY=:99
-
 git --version 
 
 #cd /home/jmeterscript
